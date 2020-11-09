68b86b5fac3a2fc4ec55a68f276d1a3ce71c0e71
caxton-m <caxymuchono@gmail.com>
2020-11-09 17:02:18 -0600

    Finished FileSize

---
+++ src/Cake.java
@@ -0,0 +1,20 @@
+
+public class Cake {
+	private int bites = 10;
+	
+	public Cake() {
+		// nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Hmm.. yummy");
+	}
+	
+	public boolean isEmpty() {
+		
+		return bites == 0;
+		// looks 
+	}
+
+}
